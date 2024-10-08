434fa1f34b75ee320f57a5d8618e27a937571bda
letyresina <leticiaresina@gmail.com>
2024-10-07 22:34:33 -0300

    ajustado musica

--- src/main/java/br/com/fiap/mp/controller/MusicController.java
+++ src/main/java/br/com/fiap/mp/controller/MusicController.java
@@ -11,6 +11,19 @@ public class MusicController {
 
     @GetMapping
     public String musica(Model model){
+
+        String letra = "Baby, you can drive my car\n" +
+                "Yes, I'm gonna be a star\n" +
+                "Baby, you can drive my car\n" +
+                "And maybe I love you\n" +
+                "\n" +
+                "I told that girl that my prospects were good\n" +
+                "And she said: Baby, it's understood\n" +
+                "Working for peanuts is all very fine\n" +
+                "But I can show you a better time";
+
+        model.addAttribute("musica", letra);
+
         return "musica";
     }
 }
